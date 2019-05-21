118d7d6403f744d4adf0d382d703f2120f971744
/)/) -- Daniel Haslinger <creo+github@blackmesa.at>
2019-05-21 20:28:08 +0200

    Added plugin support for scheduled invocation (e.g. cron)

--- spacecore.php
+++ spacecore.php
@@ -54,86 +54,97 @@ foreach($plugins as $plugin_src)
     error_log("class $plugin_classname loaded");
 }
 
-// read incoming layer6 stanza (L6 = HTTP(s) in this particular case)
-$layer6_stanza = file_get_contents("php://input");
-
-// decode the layer 6 stanza and extract the layer 7 information (the actual Telegram protocol) into an assoc. array)
-$GLOBALS['layer7_stanza'] = json_decode($layer6_stanza, true);
-
-// right now we are not sure if the stuff we received was valid JSON..
-if(json_last_error() === JSON_ERROR_NONE && $layer6_stanza != NULL)
+// determine invocation method: CLI or Serverbased?
+if(php_sapi_name() == 'cli')
 {
-    // Valid JSON encountered. Treat this as a telegram message
-    error_log("telegram:receiveMessage: VALID JSON DECODED: '$layer6_stanza'");
+    // fired up via command line. It's cron time.
+    error_log("CLI MODE: assuming scheduled invocation");
 
-    // Is the sender legit?
-    if(!isset($_GET['token']) || $_GET['token'] != $config['bot_token'])
-    {
-        error_log("telegram:SenderAuthentication: Invalid token: " . $_GET['token']);
-        exit;
-    }
-
-    // Run through all plugins and execute any preprocessing steps
+    // Run through all plugins and execute any housekeeping steps
     foreach ($object_broker->plugins as $registered_plugin) {
-        if (isset($GLOBALS['layer7_stanza']['message']['text'])) {
-            // if available, use text preprocessor
-            if (method_exists($object_broker->instance[$registered_plugin], 'router_preprocess_text')) {
-                $object_broker->instance[$registered_plugin]->router_preprocess_text();
-            }
+        if (method_exists($object_broker->instance[$registered_plugin], 'router_housekeeping')) {
+            // if available, do housekeeping (scheduled tasks, etc.)
+            $object_broker->instance[$registered_plugin]->router_housekeeping();
         }
-
-        if (isset($GLOBALS['layer7_stanza']['message']['photo'])) {
-            // if available, use photo preprocessor
-            if (method_exists($object_broker->instance[$registered_plugin], 'router_preprocess_photo')) {
-                $object_broker->instance[$registered_plugin]->router_preprocess_photo();
-            }
-        }
-
-        if (method_exists($object_broker->instance[$registered_plugin], 'router_preprocess')) {
-            // if available, use generic preprocessor
-            $object_broker->instance[$registered_plugin]->router_preprocess();
-        }
-    }
-
-    if (isset($GLOBALS['layer7_stanza']['message']['text'])) {
-        // Interpret text commands and route them to their registered classes
-        $object_broker->instance['api_routing']->route_text();
     }
 }
 else
 {
-    // Invalid JSON encountered (for whatever reason, we don't care).
-    // Treat this as plain GET/POST requests
+    // read incoming layer6 stanza (L6 = HTTP(s) in this particular case)
+    $layer6_stanza = file_get_contents("php://input");
 
-    error_log("getpost:receivePostBody: '$layer6_stanza'");
+    // decode the layer 6 stanza and extract the layer 7 information (the actual Telegram protocol) into an assoc. array)
+    $GLOBALS['layer7_stanza'] = json_decode($layer6_stanza, true);
 
-    /* TODO: Right now we're tapping into the data from the persistence module, but this is shitty in too many ways.
-    // The next step for this section will be to refactor this to ASK all registered plugins for public / private
-    // data to be exposed via plain GET/POST requests.
-    */
+    // right now we are not sure if the stuff we received was valid JSON..
+    if (json_last_error() === JSON_ERROR_NONE && $layer6_stanza != NULL) {
+        // Valid JSON encountered. Treat this as a telegram message
+        error_log("telegram:receiveMessage: VALID JSON DECODED: '$layer6_stanza'");
 
-    // -- collect information that is available to unauthenticated sessions
-    $spacestate = $object_broker->instance['core_persist']->retrieve('heralding.state');
-    $public_array = [];
-    $public_array['spacecore_api'] = $config['webapi_level'];
-    $public_array['spacestate'] = ($spacestate == 'open' ? 'open' : 'closed');
+        // Is the sender legit?
+        if (!isset($_GET['token']) || $_GET['token'] != $config['bot_token']) {
+            error_log("telegram:SenderAuthentication: Invalid token: " . $_GET['token']);
+            exit;
+        }
 
-    // -- collect information that is available to authenticated sessions only
-    $private_array = [];
+        // Run through all plugins and execute any preprocessing steps
+        foreach ($object_broker->plugins as $registered_plugin) {
+            if (isset($GLOBALS['layer7_stanza']['message']['text'])) {
+                // if available, use text preprocessor
+                if (method_exists($object_broker->instance[$registered_plugin], 'router_preprocess_text')) {
+                    $object_broker->instance[$registered_plugin]->router_preprocess_text();
+                }
+            }
 
-    // TODO: Move WebAPI tokens to the persistence module and generate them on demand
-    if(!isset($_GET['token']) || $_GET['token'] != $config['webapi_token'])
-    {
-        $private_array = ['unauthenticated'];
+            if (isset($GLOBALS['layer7_stanza']['message']['photo'])) {
+                // if available, use photo preprocessor
+                if (method_exists($object_broker->instance[$registered_plugin], 'router_preprocess_photo')) {
+                    $object_broker->instance[$registered_plugin]->router_preprocess_photo();
+                }
+            }
+
+            if (method_exists($object_broker->instance[$registered_plugin], 'router_preprocess')) {
+                // if available, use generic preprocessor
+                $object_broker->instance[$registered_plugin]->router_preprocess();
+            }
+        }
+
+        if (isset($GLOBALS['layer7_stanza']['message']['text'])) {
+            // Interpret text commands and route them to their registered classes
+            $object_broker->instance['api_routing']->route_text();
+        }
+
+    } else {
+        // Invalid JSON encountered (for whatever reason, we don't care).
+        // Treat this as plain GET/POST requests
+
+        error_log("getpost:receivePostBody: '$layer6_stanza'");
+
+        /* TODO: Right now we're tapping into the data from the persistence module, but this is shitty in too many ways.
+        // The next step for this section will be to refactor this to ASK all registered plugins for public / private
+        // data to be exposed via plain GET/POST requests.
+        */
+
+        // -- collect information that is available to unauthenticated sessions
+        $spacestate = $object_broker->instance['core_persist']->retrieve('heralding.state');
+        $public_array = [];
+        $public_array['spacecore_api'] = $config['webapi_level'];
+        $public_array['spacestate'] = ($spacestate == 'open' ? 'open' : 'closed');
+
+        // -- collect information that is available to authenticated sessions only
+        $private_array = [];
+
+        // TODO: Move WebAPI tokens to the persistence module and generate them on demand
+        if (!isset($_GET['token']) || $_GET['token'] != $config['webapi_token']) {
+            $private_array = ['unauthenticated'];
+        } else {
+            $private_array['spacestate'] = $spacestate;
+        }
+
+        // -- here we go:
+        $return_array = ['public' => $public_array, 'private' => $private_array];
+        print json_encode($return_array);
     }
-    else
-    {
-        $private_array['spacestate'] = $spacestate;
-    }
-
-    // -- here we go:
-    $return_array = ['public' => $public_array, 'private' => $private_array];
-    print json_encode($return_array);
 }
 
 ?>

f08b8ad7ede702db6b1292c52d02be9f6d97825d
damn-simple-scripts <33717982+damn-simple-scripts@users.noreply.github.com>
2019-11-11 01:33:16 +0100

    whitespaces

--- apis/telegram/init.inc.php
+++ apis/telegram/init.inc.php
@@ -54,7 +54,7 @@ class API_TELEGRAM
     public function delete_message($chat_id, $msg_id)
     {
         global $config;
-        
+
         // set parameters
         $params = [
             'chat_id'=>$chat_id,

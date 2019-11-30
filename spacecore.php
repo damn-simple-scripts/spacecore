a5eb0b04bfe7ff254598bdf7f3b2975b4d9de361
clemoo <damn-simple-scripts@users.noreply.github.com>
2019-11-30 22:52:48 +0100

    https://github.com/segmentationvault/spacecore/pull/8#discussion_r352305143

--- spacecore.php
+++ spacecore.php
@@ -322,7 +322,7 @@ else
                 {
                     $spaceapi_data_filter[$_GET['filter']] = $spaceapi_data[$_GET['filter']];
                 }else{
-                    $spaceapi_data_filter[$_GET['filter']] = "THIS KEY DOES NOT EXISTS!";
+                    $spaceapi_data_filter[$_GET['filter']] = "THIS KEY DOES NOT EXIST!";
                 }
                 print json_encode($spaceapi_data_filter);
             }
